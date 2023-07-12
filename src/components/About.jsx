219ad86e3dc17bdc291b8436fa2043b527e0c9bb
Temur Komilov <83705446+KomilovTemur@users.noreply.github.com>
2023-07-12 19:36:02 +0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,16 +14,10 @@ const About = () => {
         </div>
         <p className="text-xl mt-20">
           My main occupation is web programming. My experience in this field is high. I have also participated in major projects and StartUPs in my field. I can create websites, Frontend, Backend and desktop version of sites. I have worked with many companies, teams and clients.
-
         </p>
         <br />
         <p className="text-xl">
-          {/*Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe*/}
-          {/*reprehenderit aliquid tempore sapiente! Consequuntur quo earum*/}
-          {/*doloribus? Nostrum tempore aliquid vel doloremque minima rerum. Maxime*/}
-          {/*tenetur impedit, nesciunt nemo perferendis corrupti. Aperiam fuga*/}
-          {/*quasi voluptatem itaque iste saepe optio enim facere, quod omnis, ad*/}
-          {/*architecto, expedita distinctio doloribus molestiae alias.*/}
+          <img width="90%" src="https://camo.githubusercontent.com/f2da35b97f4de6424bef1541e9e1ebbb7733eabfa8c71a8e6e3f7a54a3e5c13f/68747470733a2f2f6769746875622d726561646d652d61637469766974792d67726170682e76657263656c2e6170702f67726170683f757365726e616d653d4b6f6d696c6f7654656d7572267468656d653d72656163742d6461726b26686964655f626f726465723d7472756526617265613d74727565" data-canonical-src="https://github-readme-activity-graph.vercel.app/graph?username=KomilovTemur&amp;theme=react-dark&amp;hide_border=true&amp;area=true" style="max-width: 100%;"/>
         </p>
       </div>
     </div>

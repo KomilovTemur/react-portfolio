40fc7a63385d9785c83cf83c73eb434647a706cc
Temur Komilov <83705446+KomilovTemur@users.noreply.github.com>
2023-07-23 07:08:02 +0000

    change title

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -100,7 +100,7 @@ const Experience = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-white">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
+          Technologies 
           </p>
           <p className="py-6">These are the technologies I've worked on</p>
         </div>

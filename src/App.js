a8946e7712692d18d24007f267f3101af6c3a378
Temur Komilov <83705446+KomilovTemur@users.noreply.github.com>
2023-12-26 07:08:33 +0000

    vercel speed insights

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
   return (
     <div>
+      <SpeedInsights />
       <NavBar />
       <Home />
       <About />
       <Portfolio />
       <Experience />
-      <Contact /> 
+      <Contact />
       <SocialLinks />
     </div>
   );

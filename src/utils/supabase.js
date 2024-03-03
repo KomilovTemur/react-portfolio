f9b2cc7176265f4fe2933cfa816e269233f01fe6
Temur Komilov <itteamdev74@gmail.com>
2024-03-03 14:11:34 +0500

    some secrets using in .env

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-
-const supabaseUrl = "https://sebcseumqpgzryssaawf.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlYmNzZXVtcXBnenJ5c3NhYXdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM1NzU3NTQsImV4cCI6MjAxOTE1MTc1NH0.vYcEroknozjxVTvoAXpdYXdleuMhV2RQ8lwWrJZCqcY";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

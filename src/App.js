c03eb72288b20ea96d4c95401aab3aba6a264e45
Temur Komilov <itteamdev74@gmail.com>
2024-03-03 15:13:59 +0500

    new route and component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Route, Routes } from "react-router-dom";
 import Messages from "./pages/Messages";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/messages" element={<Messages />} />
       </Routes>
     </div>
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+const Dashboard = () => {
+  return (
+    <div className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+      <div className="pt-20 w-full">
+        <div className="container mx-auto">
+        <h2 className="text-white text-3xl text-center">Dashbaord</h2>
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
